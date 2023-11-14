0f3a5f1ff90441817f36f9e74f7adb821c6aa27f
NoahOnFyre <noahonfyre@gmail.com>
2023-11-14 17:03:40 +0100

    Fix installer to work.

--- requirements/install.ps1
+++ requirements/install.ps1
@@ -1,10 +1,12 @@
-$filePath = Join-Path $env:USERPROFILE ".vulcan" "v.exe"
+$fileName = "v.exe"
+$folderPath = Join-Path $env:USERPROFILE ".vulcan"
+$filePath = Join-Path $folderPath $fileName
 $githubApiUrl = "https://api.github.com/repos/noahonfyre/vulcan/releases/latest"
 
 New-Item -ItemType Directory -Path $folderPath -Force
 
 $releaseInfo = Invoke-RestMethod -Uri $githubApiUrl
-$fileDownloadUrl = $releaseInfo.assets | Where-Object { $_.name -eq $fileName } | Select-Object -ExpandProperty browser_download_url
-Invoke-WebRequest -Uri $fileDownloadUrl -OutFile $filePath
 
-$env:Path += ";$folderPath"
+$fileDownloadUrl = $releaseInfo.assets | Where-Object { $_.name -eq $fileName } | Select-Object -ExpandProperty browser_download_url
+
+Invoke-WebRequest -Uri $fileDownloadUrl -OutFile $filePath
