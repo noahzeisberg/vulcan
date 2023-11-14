3025e2e5eb7a83e3e96d77d1b0ed97383a9513df
NoahOnFyre <noahonfyre@gmail.com>
2023-11-14 16:56:34 +0100

    Installer will now add the .vulcan directory to PATH.

--- requirements/install.ps1
+++ requirements/install.ps1
@@ -6,3 +6,5 @@ New-Item -ItemType Directory -Path $folderPath -Force
 $releaseInfo = Invoke-RestMethod -Uri $githubApiUrl
 $fileDownloadUrl = $releaseInfo.assets | Where-Object { $_.name -eq $fileName } | Select-Object -ExpandProperty browser_download_url
 Invoke-WebRequest -Uri $fileDownloadUrl -OutFile $filePath
+
+$env:Path += ";$folderPath"
