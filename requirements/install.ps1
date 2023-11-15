793ec356e8233e02cf146f386c4ced813ae6530a
NoahOnFyre <noahonfyre@gmail.com>
2023-11-15 21:00:46 +0100

    Change exit to return to not exit powershell but exit the script execution.

--- requirements/install.ps1
+++ requirements/install.ps1
@@ -1,7 +1,7 @@
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Warning "You need to have PowerShell running as administrator."
     Write-Warning "Please exit the installation and try to run again as administrator."
-    exit
+    return
 }
 
 Write-Output "Starting Vulcan installation..."
